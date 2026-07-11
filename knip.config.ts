c58282d7877f4b3ca67a95657404d2d7e8886fbd
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-07-11 12:33:02 +0200

    Declare CLI as Knip entrypoint

--- knip.config.ts
+++ knip.config.ts
@@ -1,6 +1,7 @@
 import { createKnipConfig } from "@ankhorage/devtools/knip";
 
 export default createKnipConfig({
+  entry: ["src/cli/index.ts"],
   ignoreFiles: [
     "eslint.config.mjs",
     "paradox.config.ts",
